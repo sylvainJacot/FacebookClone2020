030680e57dfb8141299d4ff78c2c608aa1bd0f90
coatriver <sylvain.jacot@hotmail.be>
2020-04-19 17:35:03 +1000

    cards mock array created

---
+++ facebookclone/src/components/Mock/cards-mock.js
@@ -0,0 +1,16 @@
+const cards = [
+    {
+        id: 0,
+        title: "test 1",
+        bgimage: false,
+        pathname: "/",
+    },
+    {
+        id: 2,
+        title: "test 2",
+        bgimage: false,
+        pathname: "/",
+    },
+]
+
+export default cards;
