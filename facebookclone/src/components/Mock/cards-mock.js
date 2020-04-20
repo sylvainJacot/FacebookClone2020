f044c678a7cf1aa711fb80122527704b18384523
coatriver <sylvain.jacot@hotmail.be>
2020-04-20 16:00:46 +1000

    Code snippet mapping fixed

--- facebookclone/src/components/Mock/cards-mock.js
+++ facebookclone/src/components/Mock/cards-mock.js
@@ -1,9 +1,8 @@
 import React from "react";
 import {imagesLearn} from "../Media/images";
 
-export const ReactRouterCode01 = () => {
-    return <>
-        <pre>
+const ReactRouterCode01 = (
+    <pre>
         <code className={"language-javascript"}>
             {`const Routes = [
     {
@@ -49,59 +48,8 @@ const AppRoutes = () => {
 export default AppRoutes;`}
         </code>
         </pre>
-    </>
-}
 
-export const ReactRouterCode02 = () => {
-    return <>
-        <pre>
-        <code className={"language-javascript"}>
-            {`const Routes = [
-    {
-        path:"/",
-        exact: true,
-        component: FaceBookHome
-    },
-    {
-        path:"/Learn",
-        exact: true,
-        component: CardsList
-    },
-    {
-        path:"/Learn/Routes-Learn",
-        exact: true,
-        component: RoutesLearn,
-    },
-
-]
-
-const AppRoutes = () => {
-    return <>
-        <Switch>
-            {
-                Routes.map((route) =>
-
-                    <Route
-                        path={route.path}
-                        exact={route.exact}
-                        component={route.component}
-                    />
-                )
-            }
-            <Route path="*" component={ErrorPage}/>
-
-        </Switch>
-
-    </>
-
-};
-
-
-export default AppRoutes;`}
-        </code>
-        </pre>
-    </>
-}
+);
 
 export const cards = [
     {
